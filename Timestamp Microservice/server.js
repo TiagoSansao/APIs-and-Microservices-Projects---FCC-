99f8ce54037125bd6bd03acda254428d9bcdfaf9
Tiago Sansão <tiagossansao@gmail.com>
2020-10-24 22:23:24 -0300

    Timestamp Microservice, first project.

---
+++ Timestamp Microservice/server.js
@@ -0,0 +1,53 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get("/api/timestamp/", (req, res) => {
+  let date = new Date()
+  res.json({"unix":date.getTime() ,"utc":date.toUTCString()})
+  res.send();
+})
+
+
+app.get("/api/timestamp/:time", (req, res) => {
+  let dateString = req.params.time;
+  let regEx = /[0-9]{5,}/
+  if (regEx.test(dateString)) {
+    dateString = parseInt(dateString)
+    res.json({"unix":dateString, "utc": new Date(dateString).toUTCString()})
+  }
+  let date = new Date(dateString)
+  if(date.toString() === "Invalid Date") {
+    res.json({"error":"Invalid Date"})
+  }
+  res.json({"unix":date.getTime() ,"utc":date.toUTCString()})
+  res.send();
+})
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
